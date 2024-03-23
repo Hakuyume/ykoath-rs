d9fccc3b11df2fb02e5e155bd0ce86b5fe271bab
Toru Ogawa <Hakuyume@users.noreply.github.com>
2024-03-23 12:40:21 +0900

    update example

--- examples/oath.rs
+++ examples/oath.rs
@@ -4,8 +4,20 @@ use ykoath_protocol::{calculate_all, YubiKey};
 
 #[derive(Parser)]
 struct Opts {
-    #[clap(long)]
-    name: String,
+    // #[clap(long)]
+    // name: String,
+    #[clap(subcommand)]
+    command: Command,
+}
+
+#[derive(Parser)]
+enum Command {
+    Code {
+        #[clap(long)]
+        single: bool,
+        query: Option<String>,
+    },
+    List,
 }
 
 fn main() -> anyhow::Result<()> {
@@ -17,26 +29,62 @@ fn main() -> anyhow::Result<()> {
     let yubikey = YubiKey::connect(&mut buf)?;
     yubikey.select(&mut buf)?;
 
-    // https://github.com/Yubico/yubikey-manager/blob/4.0.9/yubikit/oath.py#L57
-    // https://github.com/Yubico/yubikey-manager/blob/4.0.9/yubikit/oath.py#L225-L226
-    let challenge = (Utc::now().timestamp() / 30).to_be_bytes();
+    match opts.command {
+        Command::Code { single, query } => {
+            fn challenge() -> [u8; 8] {
+                // https://github.com/Yubico/yubikey-manager/blob/4.0.9/yubikit/oath.py#L57
+                // https://github.com/Yubico/yubikey-manager/blob/4.0.9/yubikit/oath.py#L225-L226
+                (Utc::now().timestamp() / 30).to_be_bytes()
+            }
 
-    // https://github.com/Yubico/yubikey-manager/blob/4.0.9/yubikit/oath.py#L391-L393
-    let response = yubikey
-        .calculate_all(true, &challenge, &mut buf)?
-        .into_iter()
-        .find(|response| response.name == opts.name.as_bytes())
-        .ok_or_else(|| anyhow::format_err!("no account: {}", opts.name))?;
+            // https://github.com/Yubico/yubikey-manager/blob/4.0.9/yubikit/oath.py#L391-L393
+            let mut responses = yubikey.calculate_all(true, &challenge(), &mut buf)?;
+            if let Some(query) = query {
+                responses.retain(|response| {
+                    response
+                        .name
+                        .windows(query.len())
+                        .any(|window| window == query.as_bytes())
+                });
+            }
 
-    let response = match response.inner {
-        calculate_all::Inner::Response(response) => response,
-        calculate_all::Inner::Hotp => anyhow::bail!("HOTP is not supported"),
-        calculate_all::Inner::Touch => {
-            eprintln!("Touch YubiKey ...");
-            yubikey.calculate(true, opts.name.as_bytes(), &challenge, &mut buf)?
+            if let [calculate_all::Response { name, inner, .. }] = &responses[..] {
+                let name = name.to_vec();
+                let code = match *inner {
+                    calculate_all::Inner::Response(response) => response.code(),
+                    calculate_all::Inner::Hotp => todo!(),
+                    calculate_all::Inner::Touch => {
+                        eprintln!("Touch YubiKey ...");
+                        let response = yubikey.calculate(true, &name, &challenge(), &mut buf)?;
+                        response.code()
+                    }
+                };
+                if single {
+                    println!("{code}");
+                } else {
+                    println!("{}\t{code}", name.escape_ascii());
+                };
+            } else {
+                anyhow::ensure!(
+                    !single,
+                    "Multiple matches, please make the query more specific.",
+                );
+                for response in responses {
+                    let code = match response.inner {
+                        calculate_all::Inner::Response(response) => response.code(),
+                        calculate_all::Inner::Hotp => "-".to_string(),
+                        calculate_all::Inner::Touch => "-".to_string(),
+                    };
+                    println!("{}\t{}", response.name.escape_ascii(), code);
+                }
+            }
         }
-    };
-    println!("{}", response.code());
+        Command::List => {
+            for response in yubikey.list(&mut buf)? {
+                println!("{}", response.name.escape_ascii());
+            }
+        }
+    }
 
     Ok(())
 }
